48d2ec3d9b8785a1d688e717b716931e70a659b5
Sumit Das <summit.dash01@gmail.com>
2021-12-07 23:55:40 +0530

    Employee sample controller added.

---
+++ WebAPI.Core.ForeachAsync.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebAPI.Core.ForeachAsync.Api.Models;
+
+namespace WebAPI.Core.ForeachAsync.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly string[] Employees = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Employee
+            {
+                DOB = DateTime.Now.AddDays(index),
+                Name = Employees[rng.Next(Employees.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ WebAPI.Core.ForeachAsync.Api/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebAPI.Core.ForeachAsync.Api.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+
+    }
+}
